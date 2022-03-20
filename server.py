6802657c5af9667417dce16b436b63a5cd8fed6f
mh-mobile <mh.mobiler@gmail.com>
2022-03-20 15:01:35 +0900

    server.pyの追加

---
+++ server.py
@@ -0,0 +1,43 @@
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing.pool import ThreadPool
+import socket
+import os
+
+from cv2 import add
+
+def __handle_message(args_tuple):
+    conn, addr, data_sum = args_tuple
+    while True:
+        data = conn.recv(1024)
+        data_sum = data_sum + data.decode('utf-8')
+
+        if not data:
+            break
+
+        if data_sum != '':
+            print(data_sum)
+
+def __get_myip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    return s.getsockname()[0]
+
+def main():
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    executor = ThreadPoolExecutor(max_workers=os.cpu_count())
+
+    myhost = __get_myip()
+    print('my ip address is now ....', myhost)
+    my_socket.bind((myhost, 50030))
+    my_socket.listen(1)
+
+    while True:
+        print('waiting for the connection')
+        conn, addr = my_socket.accept()
+        print('Connected by ..', addr)
+        data_sum = ''
+        executor.submit(__handle_message, (conn, add, data_sum))
+
+
+if __name__ == '__main__':
+    main()
