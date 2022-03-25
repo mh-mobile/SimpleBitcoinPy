ba5b0e0e58fff126be01f809ace835d8679b015f
mh-mobile <mh.mobiler@gmail.com>
2022-03-25 16:21:06 +0900

    Blockのデータからハッシュ値を算出する機能を追加

--- blockchain/blockchain_manager.py
+++ blockchain/blockchain_manager.py
@@ -1,4 +1,7 @@
+import hashlib
 import threading
+import json
+import binascii
 
 
 class BlockchianManager:
@@ -10,3 +13,10 @@ class BlockchianManager:
     def set_new_block(self, block):
         with self.lock:
             self.chain.append(block)
+
+    def _get_double_sha256(self, message):
+        return hashlib.sha256(hashlib.sha256(message).digest()).digest()
+
+    def get_hash(self, block):
+        block_string = json.dumps(block, sort_keys=True)
+        return binascii.hexlify(self._get_double_sha256((block_string).encode('utf-8'))).decode('ascii')
