6f7b6f256ce69d491279ecdf12b839a31ea77b76
mh-mobile <mh.mobiler@gmail.com>
2022-03-25 16:58:10 +0900

    BlockchainManagerとBlockBuilderを使ったブロックチェーンの生成処理

--- blockchain/block_builder.py
+++ blockchain/block_builder.py
@@ -1,10 +1,14 @@
-from .block import Block
+from .block import Block, GenesisBlock
 
 
 class BlockBuilder:
     def __init__(self):
         print('Initializaing BlockBuilder...')
         pass
+
+    def generate_genesis_block(self):
+        genesis_block = GenesisBlock()
+        return genesis_block
 
     def generate_new_block(self, transaction, previous_block_hash):
         new_block = Block(transaction, previous_block_hash)
--- blockchain/blockchain_manager.py
+++ blockchain/blockchain_manager.py
@@ -4,11 +4,16 @@ import json
 import binascii
 
 
-class BlockchianManager:
-    def __init__(self):
+class BlockchainManager:
+    def __init__(self, genesis_block):
         print('Initializaing BlockchainManager...')
         self.chain = []
         self.lock = threading.Lock()
+        self.__set_my_genesis_block(genesis_block)
+
+    def __set_my_genesis_block(self, block):
+        self.genesis_block = block
+        self.chain.append(block)
 
     def set_new_block(self, block):
         with self.lock:
@@ -20,3 +25,16 @@ class BlockchianManager:
     def get_hash(self, block):
         block_string = json.dumps(block, sort_keys=True)
         return binascii.hexlify(self._get_double_sha256((block_string).encode('utf-8'))).decode('ascii')
+
+    def is_valid(self, chain):
+        last_block = chain[0]
+        current_index = 1
+        while current_index < len(chain):
+            block = self.chain[current_index]
+            if block['previous_block'] != self.get_hash(last_block):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
