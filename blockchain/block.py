a0b67531c190b3c4c98914138540273e5d80870a
mh-mobile <mh.mobiler@gmail.com>
2022-03-25 16:10:38 +0900

    Blocのデータを辞書型に変換する機能を追加

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,4 +1,5 @@
 from time import time
+import json
 
 
 class Block:
@@ -6,3 +7,11 @@ class Block:
         self.timestamp = time()
         self.transaction = transaction
         self.previous_block = previous_block_hash
+
+    def to_dict(self):
+        d = {
+            "timestamp": self.timestamp,
+            "transaction": json.dumps(self.transaction),
+            "previous_block": self.previous_block,
+        }
+        return d
